0fdc04256d1372af90d194777e263d32a4ba9706
Siddique <sayedsiddique02@gmail.com>
2026-01-17 15:20:10 +0530

    wip: add the configuration annotation

--- backend/src/main/java/com/example/omdbapi/config/WebClientConfig.java
+++ backend/src/main/java/com/example/omdbapi/config/WebClientConfig.java
@@ -1,8 +1,10 @@
 package com.example.omdbapi.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Configuration
 public class WebClientConfig {
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
