40142fc10bcba0a67779699a7ca7f65dd652043f
Siddique <sayedsiddique02@gmail.com>
2026-01-17 10:53:43 +0530

    chore: installed spring boot for backend

---
+++ backend/src/test/java/com/example/omdbapi/OmdbapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.omdbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmdbapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
