822d5d4fdff7f290fbdca5c956daef437a643848
Siddique <sayedsiddique02@gmail.com>
2026-01-18 00:36:40 +0530

    wip: created a custom hook for handling pagination

--- frontend/src/hooks/usePagination.js
+++ frontend/src/hooks/usePagination.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import { useState } from 'react'
 
-export const usePagination = () => {
-  return (
-    <div>usePagination</div>
-  )
+export const usePagination = (defaultPage = 1) => {
+  const [page, setPage] = useState(defaultPage);
+  
+  function next() {
+    setPage(page + 1);
+  }
+
+  function reset() {
+    setPage(defaultPage);
+  }
+
+  return { page, next, reset };
 }
