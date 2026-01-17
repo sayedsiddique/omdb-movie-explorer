6f0d03951673655986535d4eb32b7cecff31a814
Siddique <sayedsiddique02@gmail.com>
2026-01-18 01:09:59 +0530

    refactor: disabled cors

--- backend/src/main/java/com/example/omdbapi/OmdbapiApplication.java
+++ backend/src/main/java/com/example/omdbapi/OmdbapiApplication.java
@@ -3,6 +3,9 @@ package com.example.omdbapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -12,4 +15,17 @@ public class OmdbapiApplication {
 		SpringApplication.run(OmdbapiApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // allow all endpoints
+						.allowedOrigins("http://localhost:3000") // your React dev URL
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
 }
