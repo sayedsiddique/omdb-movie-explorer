12cf559b1513452deba90b00ebf803384c74b82a
Siddique <sayedsiddique02@gmail.com>
2026-01-17 20:15:24 +0530

    wip: completed the header and footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,6 +2,14 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:p-6 dark:bg-gray-800 dark:border-gray-600">
+      <p className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        © { new Date().getFullYear() }
+        <a href="/" className="hover:text-primary-800">
+          &nbsp;OMDb API&trade;
+        </a>
+        . All Rights Reserved.
+      </p>
+    </footer>
   )
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,172 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+import Logo from "../assets/logo.png";
 
 export const Header = () => {
+  const [isMobileMenuVisible, setIsMobileMenuVisible] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+
+  const activeClasses = "block py-2 px-3 text-primary-800 hover:text-primary-1000 rounded";
+  const inactiveClasses = "block py-2 px-3 text-white hover:text-primary-800 rounded";
+
+  const darkModeIcon = <svg viewBox="0 0 24 24" fill="none" className="w-6 h-6"><path fillRule="evenodd" clipRule="evenodd" d="M17.715 15.15A6.5 6.5 0 0 1 9 6.035C6.106 6.922 4 9.645 4 12.867c0 3.94 3.153 7.136 7.042 7.136 3.101 0 5.734-2.032 6.673-4.853Z" className="fill-primary-800 group-hover:fill-white"></path><path d="m17.715 15.15.95.316a1 1 0 0 0-1.445-1.185l.495.869ZM9 6.035l.846.534a1 1 0 0 0-1.14-1.49L9 6.035Zm8.221 8.246a5.47 5.47 0 0 1-2.72.718v2a7.47 7.47 0 0 0 3.71-.98l-.99-1.738Zm-2.72.718A5.5 5.5 0 0 1 9 9.5H7a7.5 7.5 0 0 0 7.5 7.5v-2ZM9 9.5c0-1.079.31-2.082.845-2.93L8.153 5.5A7.47 7.47 0 0 0 7 9.5h2Zm-4 3.368C5 10.089 6.815 7.75 9.292 6.99L8.706 5.08C5.397 6.094 3 9.201 3 12.867h2Zm6.042 6.136C7.718 19.003 5 16.268 5 12.867H3c0 4.48 3.588 8.136 8.042 8.136v-2Zm5.725-4.17c-.81 2.433-3.074 4.17-5.725 4.17v2c3.552 0 6.553-2.327 7.622-5.537l-1.897-.632Z" className="fill-white group-hover:fill-primary-800"></path><path fillRule="evenodd" clipRule="evenodd" d="M17 3a1 1 0 0 1 1 1 2 2 0 0 0 2 2 1 1 0 1 1 0 2 2 2 0 0 0-2 2 1 1 0 1 1-2 0 2 2 0 0 0-2-2 1 1 0 1 1 0-2 2 2 0 0 0 2-2 1 1 0 0 1 1-1Z" className="fill-white group-hover:fill-primary-800"></path></svg>
+
+  const lightModeIcon = <svg viewBox="0 0 24 24" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-6 h-6"><path d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" className="fill-white group:hover:fill-primary-800 stroke-primary-800 group:hover:stroke-white"></path><path d="M12 4v1M17.66 6.344l-.828.828M20.005 12.004h-1M17.66 17.664l-.828-.828M12 20.01V19M6.34 17.664l.835-.836M3.995 12.004h1.01M6 6l.835.836" className="stroke-primary-800 group-hover:stroke-white"></path></svg>
+
+  const navigate = useNavigate();
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    const queryTerm = evt.target.search.value;
+    navigate(`/?q=${queryTerm}`);
+    evt.target.reset();
+  }
+
   return (
-    <div>Header</div>
+    <nav className="bg-slate-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:bg-slate-800">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link
+          to="/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <img src={Logo} className="h-8" alt="OMDb API Logo"
+          />
+        </Link>
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          {/* Mode Switcher Button */}
+          <button
+            type="button"
+            className="text-white focus:outline-none font-medium rounded-lg px-2.5 py-2 me-4 bg-white hover:bg-primary-800 group dark:bg-primary-800 dark:hover:bg-white"
+            onClick={() => setDarkMode(!darkMode) }
+          >
+            {darkMode ? darkModeIcon : lightModeIcon}
+          </button>
+
+          <button
+            type="button"
+            data-collapse-toggle="navbar-search"
+            aria-controls="navbar-search"
+            aria-expanded="false"
+            onClick={() =>setIsMobileMenuVisible(!isMobileMenuVisible)}
+            className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none  rounded-lg text-sm p-2.5 me-1"
+          >
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+            <span className="sr-only">Search</span>
+          </button>
+          <div className="relative hidden md:block">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+              <span className="sr-only">Search icon</span>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                id="search-navbar"
+                name="search"
+                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-slate-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                placeholder="Search..."
+              />
+            </form>
+          </div>
+          <button
+            data-collapse-toggle="navbar-search"
+            type="button"
+            onClick={() => setIsMobileMenuVisible(!isMobileMenuVisible)}
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none dark:text-gray-400 dark:hover:bg-gray-700 "
+            aria-controls="navbar-sticky"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+        </div>
+        <div
+          className={`items-center justify-between ${isMobileMenuVisible ? '':'hidden'} w-full md:flex md:w-auto md:order-1`}
+          id="navbar-search"
+        >
+          <div className="relative mt-3 md:hidden">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                id="search-navbar"
+                name="search"
+                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-slate-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                placeholder="Search..."
+              />
+            </form>
+          </div>
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 text-white">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) => isActive ? activeClasses : inactiveClasses}
+                aria-current="page"
+              >
+                Home
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
