c14720015b3ab914d77c9bd7b0882795f6938a21
Siddique <sayedsiddique02@gmail.com>
2026-01-18 00:36:07 +0530

    wip: created a custom hook for fetching apis

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
 
-export const useFetch = () => {
-  return (
-    <div>useFetch</div>
-  )
-}
+export const useFetch = (initialUrl) => {
+  const [url, setUrl] = useState(initialUrl);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if(!url) return;
+    const abortController = new AbortController();
+
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(url, { signal: abortController.signal });
+        if (!response.ok) {
+          setError('Failed to fetch data');
+          setData(null);
+        } else {
+          const result = await response.json();
+          setData(result);
+          setError(null);
+        }
+      } catch (error) {
+        if (error.name === 'AbortError') {
+        } else {
+          setError(error.message);
+          setData(null);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      abortController.abort();
+    };
+  }, [url]);
+  
+  return { data, error, isLoading, setUrl };
+};
