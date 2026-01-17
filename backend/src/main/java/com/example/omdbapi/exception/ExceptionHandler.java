c3edc7c48c7fd5101e5d7a36028dedb0c6363046
Siddique <sayedsiddique02@gmail.com>
2026-01-17 15:20:52 +0530

    chore: defined the global exception handler

---
+++ backend/src/main/java/com/example/omdbapi/exception/ExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.example.omdbapi.exception;
+
+import com.example.omdbapi.dto.response.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handle(Exception exception, HttpServletRequest request) {
+        return new ResponseEntity<ApiResponse<Void>>(new ApiResponse<>(
+                "Server not able to request the process at the moment. Please try again later.",
+                500
+        ), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
